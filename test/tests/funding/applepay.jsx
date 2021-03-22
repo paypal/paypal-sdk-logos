f9ecf71c337b828fe0a5977db36861475250de80
Steve Mask <smask@paypal.com>
2021-03-22 12:54:28 -0400

    test text

--- test/tests/funding/applepay.jsx
+++ test/tests/funding/applepay.jsx
@@ -6,9 +6,9 @@ import { node, html } from 'jsx-pragmatic/src';
 import { ApplePayLogo, LOGO_COLOR } from '../../../src';
 import { validateSVG } from '../common';
 
-describe('venmo logo rendering happy cases', () => {
+describe('Apple Pay logo rendering happy cases', () => {
 
-    it('should render the venmo logo', () => {
+    it('should render the Apple Pay logo', () => {
         const logo = (
             <ApplePayLogo logoColor={ LOGO_COLOR.WHITE } />
         );
