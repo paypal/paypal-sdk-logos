08f9187f8122c0df9acadc191dca2a2eb31a205c
aditya27marri <aditya27marri@gmail.com>
2022-03-09 15:06:13 -0800

    chore: add new multibanco apm logo with correct name (#55)

--- src/logos/index.js
+++ src/logos/index.js
@@ -26,3 +26,4 @@ export * from './boleto';
 export * from './maxima';
 export * from './mercadopago';
 export * from './multiblanco';
+export * from './multibanco';
---
+++ test/tests/funding/multibanco.jsx
@@ -0,0 +1,30 @@
+/* @flow */
+/** @jsx node */
+
+import { node, html } from 'jsx-pragmatic/src';
+
+import { MultibancoLogo, LOGO_COLOR } from '../../../src';
+import { validateSVG } from '../common';
+
+describe('mulitbanco logo rendering happy cases', () => {
+
+    it('should render the multibanco logo', () => {
+        const logo = (
+            <MultibancoLogo />
+        );
+
+        const logoHTML = logo.render(html());
+
+        validateSVG(logoHTML);
+    });
+
+    it('should render the multibanco white logo', () => {
+        const logo = (
+            <MultibancoLogo logoColor={ LOGO_COLOR.WHITE } />
+        );
+
+        const logoHTML = logo.render(html());
+
+        validateSVG(logoHTML);
+    });
+});
