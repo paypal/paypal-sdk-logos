e1d9d2e30ed0fc2a9d56bfba02c923719d30c2f1
nroman <nroman@paypal.com>
2025-02-04 10:50:15 -0600

    chore: generate CDN packages

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 git add ./dist --all
-git add ./test/screenshot/images --all
 
 # Generate the changelog; adding the latest commits
 ./node_modules/.bin/conventional-changelog -i CHANGELOG.md -s
