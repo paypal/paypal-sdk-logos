291c47456cafa432b22e880ccc70ebd9928dd3ea
hnguyen510 <hnguyen510@users.noreply.github.com>
2025-02-07 19:12:48 +0000

    chore: generate CDN packages

--- src/constants.js
+++ src/constants.js
@@ -53,5 +53,5 @@ export const LOGO_CLASS = {
   LOGO_COLOR: ("paypal-logo-color": "paypal-logo-color"),
 };
 
-const PACKAGE_VERSION = "2.2.7";
+const PACKAGE_VERSION = "2.3.0";
 export const CDN_BASE_URL = `https://www.paypalobjects.com/js-sdk-logos/${PACKAGE_VERSION}`;
